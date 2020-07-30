8a8703e5bfc29ecc65e8fd7850f932eb8eac0fd7
Pavel Yermakov <pavelak137@mail.ru>
2020-07-30 11:39:53 +0300

    Updates

---
+++ precache-manifest.4c985a46c9e4e6c22419d376c469b15f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8fb71a798c0c7c52368d100b41fa9d",
+    "url": "/Nasa-pictures/index.html"
+  },
+  {
+    "revision": "a7839b95ede7e261435f",
+    "url": "/Nasa-pictures/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "657b287741fb97257661",
+    "url": "/Nasa-pictures/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7839b95ede7e261435f",
+    "url": "/Nasa-pictures/static/js/2.4478c3ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Nasa-pictures/static/js/2.4478c3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657b287741fb97257661",
+    "url": "/Nasa-pictures/static/js/main.56e9d7a3.chunk.js"
+  },
+  {
+    "revision": "712f451fda07103aca36",
+    "url": "/Nasa-pictures/static/js/runtime-main.555ad703.js"
+  },
+  {
+    "revision": "d50e9b6c78a43b77161043acb0239d8e",
+    "url": "/Nasa-pictures/static/media/cosmos.d50e9b6c.png"
+  }
+]);
